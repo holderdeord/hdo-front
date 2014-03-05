96223e930f57171972d06d28fb5193991ac06804
andsandv <anders.olsen.sandvik@finn.no>
2014-03-05 23:59:17 +0100

    Add tests for promise for representative

--- test/backend.test.js
+++ test/backend.test.js
@@ -1,1 +1,18 @@
+var backend = require('../lib/hdo/backend');
+var assert = require('assert');
 
+describe('Backend', function(){
+    describe('representative', function(){
+        it('should reject when the value is not found', function(){
+            assert.equal(true, backend.representatives.getBySlug('no').isRejected());
+        });
+    });
+});
+
+describe('Backend', function(){
+    describe('representative', function(){
+        it('should fulfill when the value is found', function(){
+            assert.equal(true, backend.representatives.getBySlug('jes').isFulfilled());
+        });
+    });
+});
