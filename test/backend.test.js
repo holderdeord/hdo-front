a4531e95437f50607e2e9bb76ba2305270c2abdf
Jari Bakken <jari@holderdeord.no>
2014-05-26 20:43:44 +0200

    Test fix.

--- test/backend.test.js
+++ test/backend.test.js
@@ -1,23 +1,21 @@
-var backend = require('../lib/hdo/backend');
-var assert = require('assert');
-var Promise = require('bluebird');
+var backend = require('../lib/hdo/backend'),
+    assert  = require('assert'),
+    Promise = require('bluebird');
 
-Promise.onPossiblyUnhandledRejection(function (error) {
-    throw error;
-});
+Promise.onPossiblyUnhandledRejection(function (error) { throw error; });
 
 // TODO: maybe look at https://www.npmjs.org/package/assert-promise ?
 
 describe('Backend', function(){
     describe('representatives', function(){
         it('should find an existing representative', function(){
-            backend.representatives.getBySlug('jes').then(function(rep) {
+            backend.representatives.findBySlug('jes').then(function(rep) {
                 assert.equal('string', typeof rep.fullName);
             });
         });
-        
+
         it('should reject when the representative does not exist', function(){
-            backend.representatives.getBySlug('no-such-representative').then(function() {
+            backend.representatives.findBySlug('no-such-representative').then(function() {
                 assert(false, 'Expected promise to be rejected');
             }, function(error) {
                 assert.equal('no such representative', error);
