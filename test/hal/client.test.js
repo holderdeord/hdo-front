fd3ed1dedcd186fe9ad29ab86eaf45350266b95e
Jari Bakken <jari@holderdeord.no>
2014-06-01 22:51:44 +0200

    Use embedded resources if present

--- test/hal/client.test.js
+++ test/hal/client.test.js
@@ -7,12 +7,18 @@ var
 describe('HalClient', function() {
     var rootResponse     = { _links: {'ea:orders': {href: 'http://api.io/orders'}}},
         ordersResponse   = { _links: {'ea:find': {href: 'http://api.io/orders/{id}', templated: true}}},
+        customerResponse = { name: 'foo' },
+        basketResponse   = { name: 'bar'},
+        embeddedBasket   = { name: 'baz' },
         findResponse     = { _links: { 'ea:customer': { href: 'http://api.io/customer/42'},
                                        'ea:basket': {href: 'http://api.io/basket/123'}}},
-        customerResponse = { name: 'foo' },
-        basketResponse   = {name: 'bar'},
 
-        requestSpec      = [
+        findResponseWithEmbeddedCustomer = {
+            _links: { 'ea:customer': { href: 'http://api.io/customer/42'} },
+            _embedded: { 'ea:basket': embeddedBasket }
+        },
+
+        basicRequestSpec = [
           {
             rel: 'ea:orders',
             follow: [
@@ -32,6 +38,7 @@ describe('HalClient', function() {
         nock('http://api.io').get('/').reply(200, rootResponse)
                             .get('/orders').reply(200, ordersResponse)
                             .get('/orders/13').reply(200, findResponse)
+                            .get('/orders/14').reply(200, findResponseWithEmbeddedCustomer)
                             .get('/customer/42').reply(200, customerResponse)
                             .get('/basket/123').reply(200, basketResponse);
     });
@@ -50,7 +57,7 @@ describe('HalClient', function() {
             })],
         };
 
-        return api.request(requestSpec).then(function(actual) {
+        return api.request(basicRequestSpec).then(function(actual) {
             assert.equal(JSON.stringify(expected, null, '  '), JSON.stringify(actual, null, '  '));
         });
     });
@@ -63,10 +70,22 @@ describe('HalClient', function() {
         order.follow('ea:basket');
         order.follow('ea:customer');
 
-        var expected = requestSpec,
+        var expected = basicRequestSpec,
             actual   = builder.specs;
 
         assert.equal(JSON.stringify(expected, null, '  '), JSON.stringify(actual, null, '  '));
+    });
+
+    it('uses embedded resources if present', function () {
+        var api = client('http://api.io'),
+            spec = [merge(basicRequestSpec[0], {follow: [{params: {id: 14}}]})];
+
+        return api.request(spec).then(function (res) {
+            var order = res['ea:orders'][0]['ea:find'][0];
+            assert.equal(JSON.stringify(order['ea:basket'][0], null, '  '), JSON.stringify(embeddedBasket, null, '  '));
+        });
+
+
     });
 });
 
