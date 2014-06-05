00dc2a99120571a1cb47325a84c5ddee8b9015d7
Jari Bakken <jari@holderdeord.no>
2014-06-05 11:08:17 +0200

    Use HTTPS for API calls

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ nconf.argv()
     .defaults({
         PORT: 9090,
         NODE_ENV: 'dev',
-        api: 'http://www.holderdeord.no/api/',
+        api: 'https://www.holderdeord.no/api/',
         cache: 'enabled'
     });
 
